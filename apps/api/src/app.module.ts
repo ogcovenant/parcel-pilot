f77689809b6314e5d83849773f8f44854efc7ad4
Covenant Ogowale <justcovenant@outlook.com>
2026-08-23 02:23:22 +0100

    setup database module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { config } from './config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfig } from './config/database.config';
+import { AppConfig } from './config/app.config';
+import { join } from 'node:path';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -10,6 +15,29 @@ import { config } from './config';
       isGlobal: true,
       load: config,
     }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const database = configService.get<DatabaseConfig>('database');
+        const app = configService.get<AppConfig>('app');
+        const isDevelopment = app?.nodeEnv === 'developmemt';
+
+        return {
+          type: 'postgres',
+          host: database?.dbHost,
+          port: database?.dbPort,
+          username: database?.dbUser,
+          password: database?.dbPassword,
+          database: database?.dbName,
+          autoLoadEntities: true,
+          synchronize: false,
+          migrationsRun: !isDevelopment,
+          migrationsTableName: 'migrations',
+          migrations: [join(__dirname, '../database/migrations/*{.ts,.js}')],
+        };
+      },
+    }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- apps/api/src/config/app.config.ts
+++ apps/api/src/config/app.config.ts
@@ -2,8 +2,10 @@ import { registerAs } from '@nestjs/config';
 
 export interface AppConfig {
   port: number;
+  nodeEnv: string;
 }
 
 export default registerAs('app', (): AppConfig => ({
   port: parseInt(process.env.PORT as string) || 3000,
+  nodeEnv: process.env.NODE_ENV as string,
 }));
